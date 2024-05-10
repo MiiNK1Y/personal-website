46c531cbebb1c75ecaceee7ee7344831fc27393b
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-05-10 02:50:12 +0200

    formatting and some playing with js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,11 @@
 function set_language(){
-    let cur_lang = document.getElementById("set-language").innerHTML;
-
+    var cur_lang = document.getElementById("set-language").innerHTML;
+    console.log(cur_lang);
 }
+
+set_language();
+
+setTimeout(() => {
+    console.log("delaying...");
+    set_language();
+}, "2000");
