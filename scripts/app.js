7b4f2fd2f76f36d9bdd9bef0ea11002fdaa2e80d
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-05-12 02:24:09 +0200

    got language replacement to work

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,28 @@
-function set_language(){
-    var cur_lang = document.getElementById("set-language").innerHTML;
-    console.log(cur_lang);
+function setLanguage() {
+
+    // set language flags
+    // since "innerHTML" fetches the emoji, and not the HTML Doc Code, the Emoji itself is needed to check current language.
+    const flagNorEmoji = "🇳🇴";
+    const flagEngEmoji = "🇬🇧";
+
+    // HTML Doc Codes to view as consistent emoji.
+    const flagNor = "&#127475;&#127476;";
+    const flagEng = "&#127468;&#127463;";
+    const curLang = document.getElementById("language__flag").innerHTML;
+
+    if (
+        (curLang == flagEng) || 
+        (curLang == flagEngEmoji)
+    ) {
+        document.getElementById("language__flag").innerHTML = flagNor;
+        // start replacing all text boxes
+        document.getElementById("home-text-box").innerHTML = "Norwegian text here";
+    } else if (
+        (curLang == flagNor) || 
+        (curLang == flagNorEmoji)
+    ) {
+        document.getElementById("language__flag").innerHTML = flagEng;
+        // start replacing all text boxes
+        document.getElementById("home-text-box").innerHTML = "English text here";
+    }
 }
-
-set_language();
-
-setTimeout(() => {
-    console.log("delaying...");
-    set_language();
-}, "2000");
