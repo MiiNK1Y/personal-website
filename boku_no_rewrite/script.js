cf2777a598d2415894805a0520b0a87bea0083ed
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-06-08 23:19:50 +0200

    rewrite looking pretty

--- boku_no_rewrite/script.js
+++ boku_no_rewrite/script.js
@@ -1,5 +1,5 @@
 var showSources = function() {
-    let sourcesWindow = document.getElementById("source-window");
+    let sourcesWindow = document.getElementById("sources-window");
     if (sourcesWindow.style.display == "none") {
         sourcesWindow.style.display = "flex";
     } else {
@@ -7,8 +7,8 @@ var showSources = function() {
     }
 }
 
-var sourcesButton = document.getElementById("source-btn");
+var sourcesButton = document.getElementById("sources-btn");
 sourcesButton.addEventListener("click", showSources, false);
 
-var sourcesXButton = document.getElementById("source-window__x-btn");
+var sourcesXButton = document.getElementById("sources-window__x-btn");
 sourcesXButton.addEventListener("click", showSources, false);
